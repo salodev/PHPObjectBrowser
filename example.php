b3dc820d64407f707570b11a8527822260eb640a
Salomon <salojc2006@gmail.com>
2015-09-09 18:36:02 -0300

    Update example.php

--- example.php
+++ example.php
@@ -3,6 +3,5 @@
  * Example, how inspect the session status..
  */
 session_start();
-$path = &$_REQUEST['path'];
-PHPObjectBrowser::dumpVar($_SESSION, $path); die();
+PHPObjectBrowser::inspect($_SESSION);
 die();
